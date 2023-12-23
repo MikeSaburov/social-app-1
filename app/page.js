4c8fbdd7c33a71c8175c3517924ec40cc868ba00
Mikhail Saburov <saburov1985@gmail.com>
2023-12-23 23:20:18 +1100

    refactor(page): добавил сслыки в меню

--- app/page.js
+++ app/page.js
@@ -4,7 +4,24 @@ export default function Home() {
   return (
     <div className="flex mt-4 max-w-4xl mx-auto gap-4">
       <div className="w-1/3">
-        <Card>test links</Card>
+        <Card>
+          <h2>Меню</h2>
+          <a href="" className="block ">
+            Домой
+          </a>
+          <a href="" className="block ">
+            Друзья
+          </a>
+          <a href="" className="block ">
+            Закладки
+          </a>
+          <a href="" className="block ">
+            Уведомления
+          </a>
+          <a href="" className="block ">
+            Выйти
+          </a>
+        </Card>
       </div>
       <div className="grow">
         <Card>form here</Card>
