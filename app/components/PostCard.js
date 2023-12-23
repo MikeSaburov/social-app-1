a69d6ee91c29d24db2df31054ee93db5d68ecf5b
Mikhail Saburov <saburov1985@gmail.com>
2023-12-24 01:18:15 +1100

    refactor(PostCard): стилизовал изображение

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -22,9 +22,10 @@ export const PostCard = () => {
           enim. Doloremque, aut labore saepe dolorem tenetur vero dolores culpa
           nam, quod recusandae, accusantium eius sit. Nobis facere maxime magni.
         </p>
-        <div className="rounded-md overflow-hidden flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <img
-            src="https://images.unsplash.com/photo-1703309220781-4092c9a72d62?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8"
+            className="rounded-md overflow-hidden"
+            src="https://images.unsplash.com/photo-1702983100420-4f2a6eb136ea?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2NXx8fGVufDB8fHx8fA%3D%3D"
             alt=""
           />
         </div>
