ad7c42a8466583411cac052a3ce2c082a3e3ef6d
Mikhail Saburov <saburov1985@gmail.com>
2023-12-23 22:47:52 +1100

    refactor(page): стилизовал два блока

--- app/page.js
+++ app/page.js
@@ -2,12 +2,12 @@ import Card from './components/Card';
 
 export default function Home() {
   return (
-    <div className="flex mt-4 max-w-6xl mx-auto border border-emerald-500">
+    <div className="flex mt-4 max-w-4xl mx-auto gap-2">
       <div className="w-1/3">
         <Card>test links</Card>
       </div>
-      <div className="">
-        <div className="border border-black">form</div>
+      <div className="grow">
+        <Card>form here</Card>
         posts
       </div>
     </div>
