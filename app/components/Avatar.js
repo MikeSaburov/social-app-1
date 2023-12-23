d61a4098b1aab7436aa4cdbdbbd416202b65b321
Mikhail Saburov <saburov1985@gmail.com>
2023-12-24 00:58:12 +1100

    refactor(Avatar): вынес аватарку в отдельный компонент

---
+++ app/components/Avatar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Avatar = () => {
+  return (
+    <div className="w-12 rounded-full overflow-hidden">
+      <img
+        src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D"
+        alt=""
+      />
+    </div>
+  );
+};
--- app/components/PostFormCard.js
+++ app/components/PostFormCard.js
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import Card from './Card';
 
 export const PostFormCard = () => {
@@ -5,12 +6,7 @@ export const PostFormCard = () => {
     <Card>
       <div className="flex gap-2">
         <div>
-          <div className="w-14 rounded-full overflow-hidden">
-            <img
-              src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D"
-              alt=""
-            />
-          </div>
+          <Avatar />
         </div>
         <textarea
           className="grow p-3 block h-14 w-full text-sm text-gray-900 rounded-lg focus:border focus:ring-blue-500 focus:border-blue-500 outline-none"
