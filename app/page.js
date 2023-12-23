a486ff84b296793875a11a88d149957f6c587260
Mikhail Saburov <saburov1985@gmail.com>
2023-12-23 22:32:01 +1100

    feature(page): начал раметку

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Start Page</div>;
+  return (
+    <div className="flex mt-4 max-w-6xl mx-auto border border-emerald-500">
+      <div className="w-1/3">
+        <div className="border border-black">links</div>
+      </div>
+      <div className="">
+        <div className="border border-black">form</div>
+        posts
+      </div>
+    </div>
+  );
 }
