c7c56b946ffade46746f656134af913745099b98
Mikhail Saburov <saburov1985@gmail.com>
2023-12-23 22:42:32 +1100

    refactor(page): импортировал Card

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Card from './components/Card';
+
 export default function Home() {
   return (
     <div className="flex mt-4 max-w-6xl mx-auto border border-emerald-500">
       <div className="w-1/3">
-        <div className="border border-black">links</div>
+        <Card>test links</Card>
       </div>
       <div className="">
         <div className="border border-black">form</div>
