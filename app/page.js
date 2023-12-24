c8823cddd5aa6d06bbbf104d4d12615a2994c79a
Mikhail Saburov <saburov1985@gmail.com>
2023-12-24 11:08:01 +1100

    style(page): выровнял блоки сайта

--- app/page.js
+++ app/page.js
@@ -4,11 +4,11 @@ import { PostFormCard } from './components/PostFormCard';
 
 export default function Home() {
   return (
-    <div className="flex mt-4 max-w-4xl mx-auto gap-4">
-      <div className="w-1/3">
+    <div className="flex mt-4 ml-1 max-w-4xl mx-auto gap-4">
+      <div className=" w-4/12">
         <NavigationCard />
       </div>
-      <div className="grow">
+      <div className="w-8/12 mr-1">
         <PostFormCard />
         <PostCard />
       </div>
