780993c69db95691a9367a050faf4118777d1563
Mikhail Saburov <saburov1985@gmail.com>
2023-12-24 15:42:01 +1100

    style(Avatar): поменял фото, добавил ширину

--- app/components/Avatar.js
+++ app/components/Avatar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const Avatar = () => {
   return (
-    <div className="w-12 rounded-full overflow-hidden">
+    <div className="w-12 h-12 rounded-full overflow-hidden">
       <img
-        src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHx8MA%3D%3D"
+        src="https://images.unsplash.com/photo-1610186594416-2c7c0131e35d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
     </div>
