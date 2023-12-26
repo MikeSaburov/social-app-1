e78a80ef8afad27cf74ea92657c9dc3edff61002
Mikhail Saburov <saburov1985@gmail.com>
2023-12-26 23:34:39 +1100

    feature(Avatar, profile): ксастомный рзмер аватарки
    реализован разный размер аватарки, в зависимости от параметра size

--- app/components/Avatar.js
+++ app/components/Avatar.js
@@ -1,8 +1,12 @@
-import React from 'react';
-
-export const Avatar = () => {
+export const Avatar = ({ size }) => {
+  let width = 'w-12';
+  let heigth = 'h-12';
+  if (size === 'lg') {
+    width = 'w-36';
+    heigth = 'h-36';
+  }
   return (
-    <div className="w-12 h-12 rounded-full overflow-hidden">
+    <div className={`${width} ${heigth} rounded-full overflow-hidden`}>
       <img
         src="https://images.unsplash.com/photo-1610186594416-2c7c0131e35d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,20 @@
 import Card from '@/app/components/Card';
 import Layout from '../app/components/Layout';
+import { Avatar } from '@/app/components/Avatar';
 
 export default function Profile() {
   return (
     <Layout>
       <Card noPadding={true}>
-        <img
-          src="https://images.unsplash.com/photo-1621878983992-bac95a1e8dd2?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
-        />
-        Данила Мастер
+        <div className="h-36 overflow-hidden flex justify-center items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1621878983992-bac95a1e8dd2?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+        <Avatar size={'lg'} />
+
+        <div className="p-4">Данила Мастер</div>
       </Card>
     </Layout>
   );
