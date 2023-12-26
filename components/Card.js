345f93fe6e87c9e64a5a748325910e435490fe17
Mikhail Saburov <saburov1985@gmail.com>
2023-12-27 06:09:47 +1100

    refactor(Card): поправил стили

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,5 @@
 export default function Card({ children, noPadding }) {
-  let classes =
-    'bg-white shadow-md shadow-gray-300 rounded-md mb-4 overflow-hidden';
+  let classes = 'bg-white shadow-md shadow-gray-300 rounded-md mb-4 ';
 
   if (!noPadding) {
     classes += ' p-4';
