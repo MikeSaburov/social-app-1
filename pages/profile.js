28ec1a73dd75d568083212bab5502059521e2037
Mikhail Saburov <saburov1985@gmail.com>
2023-12-27 06:14:15 +1100

    style(profile): стилизовал карточку профиля

--- pages/profile.js
+++ pages/profile.js
@@ -6,7 +6,7 @@ export default function Profile() {
   return (
     <Layout>
       <Card noPadding={true}>
-        <div className="relative">
+        <div className="relative overflow-hidden rounded-md">
           <div className="h-36 overflow-hidden flex justify-center items-center ">
             <img
               src="https://images.unsplash.com/photo-1621878983992-bac95a1e8dd2?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
