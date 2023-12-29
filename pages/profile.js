a4d5ee524643a6ec31ad69ddd2012f284372a8bd
Mikhail Saburov <saburov1985@gmail.com>
2023-12-29 23:26:41 +1100

    style(profile): заменил фото

--- pages/profile.js
+++ pages/profile.js
@@ -195,7 +195,7 @@ export default function ProfilePage() {
 
               <div className="rounded-md overflow-hidden h-36 flex items-center shadow-md">
                 <img
-                  src="https://images.unsplash.com/photo-1577055696946-fb4ecc2835e0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="https://images.unsplash.com/photo-1556610961-2fecc5927173?q=80&w=1067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt=""
                 />
               </div>
