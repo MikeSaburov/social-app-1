5cfea6f46cea4e307879220cd40d45d2adc7bcaf
Mikhail Saburov <saburov1985@gmail.com>
2023-12-29 23:34:07 +1100

    feature(saved): создал страницу SavedPostPage

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -62,7 +62,7 @@ export const NavigationCard = () => {
           </svg>
           Друзья
         </Link>
-        <Link href="" className={nonActiveElementClasses}>
+        <Link href="/saved" className={nonActiveElementClasses}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
