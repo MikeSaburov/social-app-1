d9921b8a7b97a7ab9a914b108a403cd7ebfcd1b8
Mikhail Saburov <saburov1985@gmail.com>
2023-12-29 23:36:23 +1100

    refactor(saved): добавил карточки постов

--- pages/saved.js
+++ pages/saved.js
@@ -1,5 +1,12 @@
 import Layout from '@/components/Layout';
+import { PostCard } from '@/components/PostCard';
 
-export default function SavedPostPAge() {
-  return <Layout>SavedPage</Layout>;
+export default function SavedPostPage() {
+  return (
+    <Layout>
+      <h1>Твои сохраненные посты</h1>
+      <PostCard />
+      <PostCard />
+    </Layout>
+  );
 }
