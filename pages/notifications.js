0d6d134d65253b00fc590868b519879d3b29f119
Mikhail Saburov <saburov1985@gmail.com>
2023-12-30 14:50:22 +1100

    style(notifications): стилизовал страницу уведомлений

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,10 +1,77 @@
+import { Avatar } from '@/components/Avatar';
 import Card from '@/components/Card';
 import Layout from '@/components/Layout';
+import Link from 'next/link';
 
 export default function Notifications() {
   return (
     <Layout>
       <h1 className="text-3xl mb-4 text-gray-400">Уведомления</h1>
+      <Card noPadding={true}>
+        <div className="flex gap-2 items-center border-b border-b-gray-100 p-4">
+          <Link
+            href="/profile"
+            className="hover:scale-125 transition ease-in cursor-pointer"
+          >
+            <Avatar />
+          </Link>
+
+          <div className="text-sm">
+            <Link
+              href="/profile"
+              className="font-semibold hover:underline mr-1"
+            >
+              Данила Мастер
+            </Link>
+            лайкнул
+            <Link href="" className="text-socialBlue hover:underline ml-1">
+              твое фото
+            </Link>
+          </div>
+        </div>
+        <div className="flex gap-2 items-center border-b border-b-gray-100 p-4">
+          <Link
+            href="/profile"
+            className="hover:scale-125 transition ease-in cursor-pointer"
+          >
+            <Avatar />
+          </Link>
+
+          <div className="text-sm">
+            <Link
+              href="/profile"
+              className="font-semibold hover:underline mr-1"
+            >
+              Данила Мастер
+            </Link>
+            лайкнул
+            <Link href="" className="text-socialBlue hover:underline ml-1">
+              твое фото
+            </Link>
+          </div>
+        </div>
+        <div className="flex gap-2 items-center border-b border-b-gray-100 p-4">
+          <Link
+            href="/profile"
+            className="hover:scale-125 transition ease-in cursor-pointer"
+          >
+            <Avatar />
+          </Link>
+
+          <div className="text-sm">
+            <Link
+              href="/profile"
+              className="font-semibold hover:underline mr-1"
+            >
+              Данила Мастер
+            </Link>
+            лайкнул
+            <Link href="" className="text-socialBlue hover:underline ml-1">
+              твое фото
+            </Link>
+          </div>
+        </div>
+      </Card>
     </Layout>
   );
 }
