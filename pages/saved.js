2c477e19fbd168be39992e944fbc78a495761c22
Mikhail Saburov <saburov1985@gmail.com>
2023-12-30 14:23:39 +1100

    style(saved): стилизовал заголовок

--- pages/saved.js
+++ pages/saved.js
@@ -4,7 +4,7 @@ import { PostCard } from '@/components/PostCard';
 export default function SavedPostPage() {
   return (
     <Layout>
-      <h1>Твои сохраненные посты</h1>
+      <h1 className="text-3xl mb-4 text-gray-300">Твои сохраненные посты</h1>
       <PostCard />
       <PostCard />
     </Layout>
