8f8716de65b6935defa159e600dc857f9f15625a
Mikhail Saburov <saburov1985@gmail.com>
2023-12-30 15:38:27 +1100

    fix(login): убрал скролл

--- pages/login.js
+++ pages/login.js
@@ -4,8 +4,8 @@ import Layout from '@/components/Layout';
 export default function LoginPage() {
   return (
     <Layout hideNavigation={true}>
-      <div className="h-screen flex items-center">
-        <div className="max-w-md grow mx-auto -mt-24">
+      <div className="h-screen flex items-center -mt-4">
+        <div className="max-w-md grow mx-auto ">
           <Card>Login Form</Card>
         </div>
       </div>
