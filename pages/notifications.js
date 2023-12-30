1c692a7737b9502bba99255f3c1860c2bb9b16df
Mikhail Saburov <saburov1985@gmail.com>
2023-12-30 14:32:14 +1100

    style(notifications, saved): стилизовал заголовки страниц

--- pages/notifications.js
+++ pages/notifications.js
@@ -4,7 +4,7 @@ import Layout from '@/components/Layout';
 export default function Notifications() {
   return (
     <Layout>
-      <Card>Страница Уведомлений</Card>
+      <h1 className="text-3xl mb-4 text-gray-400">Уведомления</h1>
     </Layout>
   );
 }
