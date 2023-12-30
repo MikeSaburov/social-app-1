92e60ec1f5c442a217ceb84f8e11acf055a49b0f
Mikhail Saburov <saburov1985@gmail.com>
2023-12-30 21:38:41 +1100

    feature(login): сверстал страницу входа через соц.сети

--- pages/login.js
+++ pages/login.js
@@ -4,21 +4,43 @@ import Layout from '@/components/Layout';
 export default function LoginPage() {
   return (
     <Layout hideNavigation={true}>
-      <div className="h-screen flex items-center -mt-4">
-        <div className="max-w-md grow mx-auto ">
+      <div className="h-screen flex items-center -mt-24">
+        <div className="max-w-xs grow mx-auto ">
+          <h1 className="text-5xl text-gray-400 mb-4 text-center">Вход</h1>
           <Card>
-            <h1 className="text-sm">Имя пользователя</h1>
-            <div>
+            <a href="" className="flex gap-3 items-center justify-center p-4">
               <svg
+                className="h-8"
                 xmlns="http://www.w3.org/2000/svg"
-                height="16"
-                width="15.25"
                 viewBox="0 0 488 512"
               >
                 <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
               </svg>
-              <a href="">Войти через Google</a>
-            </div>
+              <p className="min-w-40">Войти через Google</p>
+            </a>
+
+            <a href="" className="flex gap-3 items-center justify-center p-4">
+              <svg
+                className="h-8"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+              >
+                <path d="M31.5 63.5C0 95 0 145.7 0 247V265C0 366.3 0 417 31.5 448.5C63 480 113.7 480 215 480H233C334.3 480 385 480 416.5 448.5C448 417 448 366.3 448 265V247C448 145.7 448 95 416.5 63.5C385 32 334.3 32 233 32H215C113.7 32 63 32 31.5 63.5zM75.6 168.3H126.7C128.4 253.8 166.1 290 196 297.4V168.3H244.2V242C273.7 238.8 304.6 205.2 315.1 168.3H363.3C359.3 187.4 351.5 205.6 340.2 221.6C328.9 237.6 314.5 251.1 297.7 261.2C316.4 270.5 332.9 283.6 346.1 299.8C359.4 315.9 369 334.6 374.5 354.7H321.4C316.6 337.3 306.6 321.6 292.9 309.8C279.1 297.9 262.2 290.4 244.2 288.1V354.7H238.4C136.3 354.7 78 284.7 75.6 168.3z" />
+              </svg>
+
+              <p className="min-w-40"> Войти через VK</p>
+            </a>
+
+            <a href="" className="flex gap-3 items-center justify-center p-4">
+              <svg
+                className="h-8"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256v32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32v80 32c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-106-86-192-192-192zm64 192a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z" />
+              </svg>
+              <p className="min-w-40">Войти через Mail</p>
+            </a>
           </Card>
         </div>
       </div>
