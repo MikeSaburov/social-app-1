827e1c3d81c3a20c7525d69039bc30c735ba4da8
Mikhail Saburov <saburov1985@gmail.com>
2023-12-31 20:55:10 +1100

    refactor(PostFormCard): адаптировал иконки
    На маленьких экранах текст скрывается, остаются только иконки

--- components/PostFormCard.js
+++ components/PostFormCard.js
@@ -32,7 +32,7 @@ export const PostFormCard = () => {
                 d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
               />
             </svg>
-            Люди
+            <span className="hidden md:block"> Люди</span>
           </button>
         </div>
         <div>
@@ -56,7 +56,7 @@ export const PostFormCard = () => {
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
               />
             </svg>
-            Геопозиция
+            <span className="hidden md:block"> Геопозиция</span>
           </button>
         </div>
         <div>
@@ -75,7 +75,7 @@ export const PostFormCard = () => {
                 d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
               />
             </svg>
-            Настроение
+            <span className="hidden md:block"> Настроение</span>
           </button>
         </div>
         <div className="grow text-right">
