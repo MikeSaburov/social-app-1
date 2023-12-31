60e12fcc0fb85225fe8b107d50711e134eea7148
Mikhail Saburov <saburov1985@gmail.com>
2023-12-31 16:04:48 +1100

    refactor(Layout): адаптировал блок с контентом

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,14 @@ import { NavigationCard } from './NavigationCard';
 import Head from 'next/head';
 
 export default function Layout({ children, hideNavigation }) {
+  let rightColumnClasses = '';
+
+  if (hideNavigation) {
+    rightColumnClasses += 'w-full';
+  } else {
+    rightColumnClasses += 'md:w-9/12 md:mx-2 mx-4';
+  }
+
   return (
     <div>
       <Head>
@@ -12,16 +20,14 @@ export default function Layout({ children, hideNavigation }) {
         />
         <link rel="icon" href="/icon.png" />
       </Head>
-      <div className="flex mt-4  max-w-4xl mx-auto gap-4">
+      <div className="md:flex mt-4  max-w-4xl mx-auto gap-4">
         {!hideNavigation && (
           <div className=" w-3/12 ml-1">
             <NavigationCard />
           </div>
         )}
 
-        <div className={hideNavigation ? 'w-full' : 'w-9/12 mr-1'}>
-          {children}
-        </div>
+        <div className={rightColumnClasses}>{children}</div>
       </div>
     </div>
   );
