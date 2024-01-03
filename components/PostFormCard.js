e4ef3d41b6376d3f8e3092601c1c8af85d500062
Mikhail Saburov <saburov1985@gmail.com>
2024-01-03 21:28:12 +1100

    refactor(PostFormCard): отображение блока если данные пришли с сервера
    проверяем, если данные подгрузились, то отображаем texarea

--- components/PostFormCard.js
+++ components/PostFormCard.js
@@ -30,12 +30,14 @@ export const PostFormCard = () => {
         <div>
           <Avatar url={profile.avatar} />
         </div>
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="grow p-3 block h-14 w-full text-sm text-gray-900 rounded-lg focus:border focus:ring-blue-500 focus:border-blue-500 outline-none"
-          placeholder={`Что ты хочешь рассказать, ${profile.name}?`}
-        ></textarea>
+        {profile.name && (
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="grow p-3 block h-14 w-full text-sm text-gray-900 rounded-lg focus:border focus:ring-blue-500 focus:border-blue-500 outline-none"
+            placeholder={`Что ты хочешь рассказать, ${profile.name}?`}
+          ></textarea>
+        )}
       </div>
 
       {/* Блок кнопок */}
