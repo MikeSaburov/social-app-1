3a85958b5719b118113c45cab9d46ad02696134f
Mikhail Saburov <saburov1985@gmail.com>
2024-01-03 22:26:53 +1100

    feature(index): получаем все посты из БД

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import { useSession } from '@supabase/auth-helpers-react';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import Layout from '../components/Layout';
 import { PostCard } from '../components/PostCard';
 import { PostFormCard } from '../components/PostFormCard';
 import LoginPage from './login';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const supabase = useSupabaseClient();
   const session = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    supabase
+      .from('posts')
+      .select()
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
 
   if (!session) {
     return <LoginPage />;
