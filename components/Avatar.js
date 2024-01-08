5217d28ba628576dc10f0b960c4b25d0a90489ec
Mikhail Saburov <saburov1985@gmail.com>
2024-01-08 20:41:54 +1100

    style(Avatar): стилизовано отображение аватара

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,12 +2,12 @@ export const Avatar = ({ size, url }) => {
   let width = 'w-12';
   let heigth = 'h-12';
   if (size === 'lg') {
-    width = 'w-24 md:w-36';
-    heigth = 'h-24 md:h-36';
+    width = 'w-24 md:w-32';
+    heigth = 'h-24 md:h-32';
   }
   return (
     <div className={`${width} ${heigth} rounded-full overflow-hidden`}>
-      <img src={url} alt="" />
+      <img src={url} alt="" className="w-full" />
     </div>
   );
 };
