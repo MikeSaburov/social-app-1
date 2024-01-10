ec75a51ad86bbf7c49b57c9d779e518678e57acc
Mikhail Saburov <saburov1985@gmail.com>
2024-01-10 20:21:02 +1100

    refactor(Avatar): зменил иконку смены аватрки

--- components/Avatar.js
+++ components/Avatar.js
@@ -11,7 +11,7 @@ export const Avatar = ({ size, url, editable, onChange }) => {
         <img src={url} alt="" className="w-full" />
       </div>
       {editable && (
-        <button className="absolute bottom-0 right-0 shadow-md shadow-gray-300 p-1 bg-white rounded-md">
+        <button className="absolute bottom-0 right-0 shadow-md shadow-gray-500 p-1 bg-white rounded-full">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -23,7 +23,12 @@ export const Avatar = ({ size, url, editable, onChange }) => {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+              d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
             />
           </svg>
         </button>
