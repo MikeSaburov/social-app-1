3bc25b69a3508afc914fd76cadb2acb831c739ff
Mikhail Saburov <saburov1985@gmail.com>
2024-01-13 18:15:47 +1100

    feature(profile):добавил кнопку Отмена

--- pages/profile.js
+++ pages/profile.js
@@ -125,44 +125,55 @@ export default function ProfilePage() {
                 )}
                 {!editMode && (
                   <div className="text-gray-500 leading-4 text-sm md:text-xl ">
-                    {profile?.place || 'Млечный Путь. Земля:)'}
+                    {profile?.place || 'Неизвестное откуда'}
                   </div>
                 )}
               </div>
               <div>
-                {isMyUser && !editMode && (
-                  <button
-                    onClick={() => {
-                      setEditMode(true);
-                      setName(profile.name);
-                      setPlace(profile.place);
-                    }}
-                    className="rounded-full border shadow-sm shadow-gray-300 text-sm px-2 py-1 hover:bg-socialBlue hover:text-white hover:bg-opacity-90"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
+                <div className="flex gap-2 ">
+                  {isMyUser && !editMode && (
+                    <button
+                      onClick={() => {
+                        setEditMode(true);
+                        setName(profile.name);
+                        setPlace(profile.place);
+                      }}
+                      className="rounded-full border shadow-sm shadow-gray-300 text-sm px-2 py-1 hover:bg-socialBlue hover:text-white hover:bg-opacity-90"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                      />
-                    </svg>
-                  </button>
-                )}
-                {isMyUser && editMode && (
-                  <button
-                    onClick={saveProfile}
-                    className="rounded-md border shadow-sm shadow-gray-300 text-sm px-2 py-1  hover:bg-socialBlue hover:bg-opacity-80 hover:text-white "
-                  >
-                    Сохранить
-                  </button>
-                )}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                        />
+                      </svg>
+                    </button>
+                  )}
+                  {isMyUser && editMode && (
+                    <button
+                      onClick={saveProfile}
+                      className="rounded-md border shadow-sm shadow-gray-300 text-sm px-2 py-1  hover:bg-socialBlue hover:bg-opacity-80 hover:text-white "
+                    >
+                      Сохранить
+                    </button>
+                  )}
+
+                  {isMyUser && editMode && (
+                    <button
+                      onClick={saveProfile}
+                      className="rounded-md border shadow-sm shadow-gray-300 text-sm px-2 py-1  hover:bg-socialBlue hover:bg-opacity-80 hover:text-white "
+                    >
+                      Отмена
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
 
