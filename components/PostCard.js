7379907b4b621a57357eddad3e6978a27f122f2b
Mikhail Saburov <saburov1985@gmail.com>
2024-01-15 22:51:01 +1100

    feature(PostCard): изменение цвета и кол-ва лайков

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,7 +2,7 @@
 
 import Card from './Card';
 import { Avatar } from './Avatar';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import ClickOutHandler from 'react-clickout-handler';
 import { useState } from 'react';
 import Link from 'next/link';
@@ -19,10 +19,21 @@ export default function PostCard({
   profiles: authorProfile,
 }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [likes, setLikes] = useState([]);
 
   const { profile: myProfile } = useContext(UserContext);
 
   const supabase = useSupabaseClient();
+
+  useEffect(() => {
+    supabase
+      .from('likes')
+      .select()
+      .eq('post_id', id)
+      .then((res) => {
+        setLikes(res.data);
+      });
+  }, []);
 
   function openDropdown() {
     setDropdownOpen(!dropdownOpen);
@@ -33,7 +44,23 @@ export default function PostCard({
     setDropdownOpen(false);
   }
 
-  function likeThisPost() {}
+  const isLikedByMe = !!likes.find((like) => like.user_id === myProfile.id);
+
+  function likeThisPost() {
+    if (isLikedByMe) {
+      return;
+    }
+
+    supabase
+      .from('likes')
+      .insert({
+        post_id: id,
+        user_id: myProfile.id,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
 
   return (
     <Card>
@@ -220,14 +247,14 @@ export default function PostCard({
       </div>
 
       <div className="mt-4 flex gap-6 text-xs">
-        <button className="flex gap-1 items-center">
+        <button className="flex gap-1 items-center" onClick={likeThisPost}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className={'w-6 h-6 ' + (isLikedByMe ? 'fill-red-500' : '')}
           >
             <path
               strokeLinecap="round"
@@ -235,9 +262,9 @@ export default function PostCard({
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
             />
           </svg>
-          0
+          {likes.length}
         </button>
-        <button className="flex gap-1 items-center" onClick={likeThisPost}>
+        <button className="flex gap-1 items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
