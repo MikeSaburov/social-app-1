023e5d73a6344c3767c3f10e7ca6e83a8c12cd01
Mikhail Saburov <saburov1985@gmail.com>
2024-01-18 21:30:58 +1100

    feature(saved): частично реализовано добавление постов в закладки

--- pages/saved.js
+++ pages/saved.js
@@ -1,13 +1,35 @@
 import Layout from '@/components/Layout';
-import { PostCard } from '@/components/PostCard';
-import { useState } from 'react';
+import PostCard from '@/components/PostCard';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
 
 export default function SavedPostPage() {
   const [posts, setPosts] = useState([]);
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  useEffect(() => {
+    if (!session?.user?.id) {
+      return;
+    }
+    supabase
+      .from('saved_posts')
+      .select('*, profiles(*)')
+      .eq('user_id', session?.user?.id)
+      .then((res) => {
+        setPosts(res.data);
+      });
+  }, []);
 
   return (
     <Layout>
       <h1 className="text-3xl mb-4 text-gray-400">Твои сохраненные посты</h1>
+      {posts.length &&
+        posts.map((post) => (
+          <div key={post.id}>
+            <PostCard {...post} />
+          </div>
+        ))}
     </Layout>
   );
 }
